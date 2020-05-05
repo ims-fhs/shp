c92d38bcf79ac61a819ed615b84a519524010e38
Adrian <trompetenschmid@gmail.com>
2020-05-05 15:40:19 +0200

    dsl-v1.R: cross_sectional() und explain() als erste Versionen

--- R/dsl-v1.R
+++ R/dsl-v1.R
@@ -45,15 +45,44 @@ longitudinal <- function(data, var_name, path. = path) {
   return(data)
 }
 
-# cross_sectional <- function(data, path = check_path(path), var_name) {
-#   cross_sectional <- import_cols("SHP10_P_USER.sav", "../data/rawdata/Data SPSS/SHP-Data-W1-W19-SPSS/W12_2010", cols = c("IDPERS", "AGE10"))
+#' cross_sectional: import a cross-sectional variable
+#'
+#' @param data
+#' @param var_name
+#' @param path.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' imsbasics::clc()
+#' library(tidyverse); library(shp); library(kml)
+#' path <- "data/rawdata/Data SPSS"
+#' open(path) %>% longitudinal("PXXF50") %>% cross_sectional("AGE10")
+#' open(path) %>% longitudinal("PXXF50") %>% cluster() %>% cross_sectional("AGE10")
+#' data <- open(path) %>% longitudinal("PXXF50") %>% cluster() %>% cross_sectional("AGE10")
+cross_sectional <- function(data, var_name, path. = path) {
+  # browser()
+  path. <- paste0(path., "/SHP-Data-W1-W19-SPSS")
+  year <- readr::parse_number(var_name)
+
+  file <- paste0("SHP", as.character(sprintf('%02d', year %% 100)), "_P_USER.sav")
+  if(year < 50) year <- paste0("20", year) else year <- paste0("19", year)
+  path. <- paste0(path., "/W", as.character(as.numeric(year) - 1998), "_", as.character(year))
+
+  df <- import_cols(file, path., cols = c("IDPERS", var_name))
+  names(df)[1] <- "ID"
+
+#
+#   cross_sectional <- import_cols(
+#     paste0("SHP", year, "_P_USER.sav"), paste0("../data/rawdata/Data SPSS/SHP-Data-W1-W19-SPSS/W12_2010", cols = c("IDPERS", "AGE10"))
 #   names(age)[1] <- "ID"
-#   data <- left_join(data, age)
-#   return(data)
-# }
+  data <- left_join(data, df)
+  return(data)
+}
 
 
-#' longitudinal: import a longitudinal variable, the longest available sequence will  be taken
+#' cluster
 #'
 #' @param data
 #' @param path
@@ -66,17 +95,51 @@ longitudinal <- function(data, var_name, path. = path) {
 #' imsbasics::clc()
 #' library(tidyverse); library(shp); library(kml)
 #' path <- "data/rawdata/Data SPSS"
-#' open(path) %>% longitudinal("PXXF50") %>% cluster()
+#' data <- open(path) %>% longitudinal("PXXF50") %>% cluster()
+#' data <- open(path) %>% longitudinal("PXXF50") %>% cluster(interactive = TRUE)
+#' data <- open(path) %>% longitudinal("PXXF50") %>% cluster(nr_clusters = 5)
 #' open(path) %>% longitudinal("PXXC75") %>% cluster()
-cluster <- function(data, nr_clusters = c(2:6)) {
+cluster <- function(data, nr_clusters = c(2:6), interactive = FALSE) {
+  # browser()
+  res <- data %>% filter(complete.cases(.))
   data <- data %>% filter(complete.cases(.))
   data <- clusterLongData(data)
   # res <- kml(data, nr_clusters, parAlgo = parKml(saveFreq = Inf))
-  res <- kml(data, nr_clusters)
+  kml(data, nr_clusters)
   data@criterionActif <- "Ray.Turi"
-  x11(type = "Xlib"); choice(data)
-  return(data)
+  if (interactive) { x11(type = "Xlib"); choice(data) }
+  res$cluster <- as.character(getClusters(data, nr_clusters[1]))
+  return(res)
 }
 
-# explain <- function(data) {
-# }
+#' Title
+#'
+#' @param data
+#' @param what
+#' @param with
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' imsbasics::clc()
+#' library(tidyverse); library(shp); library(kml)
+#' path <- "data/rawdata/Data SPSS"
+#' open(path) %>% longitudinal("PXXF50") %>% cluster() %>% cross_sectional("SEX10") %>% explain()
+#' open(path) %>% longitudinal("PXXF50") %>% cluster() %>% cross_sectional("AGE10") %>% explain()
+#' data <- open(path) %>% longitudinal("PXXF50") %>% cluster()
+#' data %>% cross_sectional("AGE10") %>% explain()
+#' data %>% cross_sectional("SEX10") %>% explain()
+explain <- function(data, what = cluster, with = as.name(names(data)[length(names(data))])) {
+  if (length(unique(data[[with]])) < 5) {
+    what <- enquo(what)
+    with <- enquo(with)
+
+    table(select(data, !!what, !!with))
+  } else {
+    what <- enquo(what)
+    with <- enquo(with)
+
+    data %>% group_by(!!what) %>% summarise(!!with := mean(!!with))
+  }
+}
