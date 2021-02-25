cdd4996e3e0062e1526407d0a16ebe080a90016f
Michi <michael.schmid@fhsg.ch>
2021-02-25 15:47:35 +0100

    Adapt Fabian's script to new loadd-path.

--- Scripts_Fabian/BATH_Fabian.R
+++ Scripts_Fabian/BATH_Fabian.R
@@ -1,266 +1,104 @@
-#install.packages('tidyverse')
 library(tidyverse)
+library(shp)
+load_path <- paste0("data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS")
 
-#' import_SPSS_file
-#'
-#' @param file
-#' @param path
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' shp99_p_user <- import_SPSS_file("SHP99_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W1_1999")
-import_SPSS_file <- function(file = stop("SHP99_P_USER.sav"),
-                             path = stop("~/FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/W1_1999/SHP99_P_USER.sav")) {
-  df <- foreign::read.spss(paste0(getwd(), "/", path, "/", file), use.value.labels = TRUE, to.data.frame = TRUE, max.value.labels = -1)
-  assertthat::assert_that(
-    assertthat::are_equal(class(df), "data.frame")
-  )
-  return(df)
-}
-
-
-
-
-#' import_SPSS_file_head
-#'
-#' @param file
-#' @param path
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' shp99_h_user_head <- import_SPSS_file_head("SHP99_H_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W1_1999")
-#' shp99_p_user_head <- import_SPSS_file_head("SHP99_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W1_1999")
-#' shp04_p_user_head <- import_SPSS_file_head("SHP04_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W6_2004")
-import_SPSS_file_head <- function(file = stop("SHP99_P_USER.sav"),
-                                  path = stop("~/FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/W1_1999/SHP99_P_USER.sav")) {
-  df <- foreign::read.spss(paste0(getwd(), "/", path, "/", file), use.value.labels = TRUE, to.data.frame = TRUE, max.value.labels = -1)
-  assertthat::assert_that(
-    assertthat::are_equal(class(df), "data.frame")
-  )
-  return(df[1:5, ])
-}
-
-
-
-
-#' import_id
-#'
-#' @param file
-#' @param path
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' shp99_p_user_id <- import_id("SHP99_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W1_1999")
-import_id <- function(file = stop("SHP99_P_USER.sav"),
-                      path = stop("~/FHS/5. Semester/BATHV/dataset_932                                                                     (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/"))
-{
-  df <- import_SPSS_file(file, path)
-  df <- df[, 2]
-  return(df)
-}
-
-
-
-
-#' import_cols
-#'
-#' @param file
-#' @param path
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' shp99_p_user_cols23 <- import_cols("SHP99_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W1_1999", cols = c(2,3))
-#' difficulties to conciliate personal and professional life last 12 months
-#' shp99_p_user_cols_id_p99f09 <- import_cols("SHP99_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W1_1999", cols = c("IDPERS", "P99F09"))
-#' shp04_p_user_cols_id_p04w604 <- import_cols("SHP04_P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS/W6_2004", cols = c("IDPERS", "P04W604"))
-import_cols <- function(file = stop("SHP99_P_USER.sav"),
-                        path = stop("~/FHS/5. Semester/BATHV/dataset_932                                                                     (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/"),
-                        cols = stop(c("IDPERS", "P04W604")))
-
-{
-  df <- import_SPSS_file(file, path)
-  assertthat::assert_that(all(cols %in% names(df)))
-  df <- df[, cols]
-  assertthat::assert_that(
-    assertthat::are_equal(ncol(df), length(cols))
-  )
-  return(df)
-}
-
-
-#' import_long_cols
-#'
-#' @param file
-#' @param path
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' library(tidyverse)
-#' shp_p_user_cols_id_p99f09 <- import_long_cols("P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS", cols = c("IDPERS", "PXXF09"))
-#' shp_p_user_cols_id_p99f09 <- import_long_cols("P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS", cols = c("IDPERS", "PXXF09"), year_start = "1999", year_end = "2003")
-#' # work conditions stress
-#' shp_p_user_cols_id_w604 <- import_long_cols("P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS", cols = c("IDPERS", "P04W604"), year_start = "2004", year_end = "2004")
-#' shp_p_user_cols_id_w604 <- import_long_cols("P_USER.sav", "data/rawdata/Data SPSS/SHP-Data-W1-W20-SPSS", cols = c("IDPERS", "P04W604"), year_start = "2004", year_end = "2017")
-#' # interference work - private
 
 
 
 
 
 
-import_long_cols <- function(file = stop("P_USER.sav"),
-                             path = stop("~/FHS/5. Semester/BATHV/dataset_932(2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/"),
-                             cols = c("IDPERS", "PXXW604"),
-                             year_start = "1999",
-                             year_end = "2018")
-{
-  years <- as.numeric(year_start):as.numeric(year_end)
-  data <- list()
-  for(i in seq_along(years))
-  {
-    file_i <- paste0("SHP", as.character(sprintf('%02d', years[i] %% 100)), "_", file)
-    path_i <- paste0(path, "/W", as.character(as.numeric(years[i]) - 1998), "_", as.character(years[i]))
 
 
 
-    cat(paste0("Importing year ", years[i], "...  "))
-    df <- import_cols(file_i, path_i, cols = yearly_col_names(cols, years[i]))
-    names(df)[1] <- "ID"
-    data[[i]] <- df
-  }
-  return(data %>% reduce(left_join, by = "ID"))
-}
 
 
 
 
-#' Title
-#'
-#' @param cols
-#' @param year
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' yearly_col_names(c("IDPERS", "AYYA00"), 2012)
-#' yearly_col_names(c("IDPERS", "SEXYY"), 2012)
-#' yearly_col_names(c("IDHOUS", "H$$H01"), 2012)
-yearly_col_names <- function(cols, year) {
-  assertthat::assert_that(assertthat::is.string(cols[1]))
-  assertthat::assert_that(assertthat::is.number(year))
-  assertthat::assert_that(assertthat::see_if(year >= 1999))
-  assertthat::assert_that(assertthat::see_if(year <= 2020))
-  # assertthat::assert_that(assertthat::are_equal(cols[1], "IDPERS"))
 
 
 
-  if(cols[1] == "IDPERS") {
-    for (i in 2:length(cols)) {
-      cols[i] <- paste0(stringr::str_split(cols[i], "YY")[[1]][1], as.character(sprintf('%02d', year %% 100)), stringr::str_split(cols[i], "YY")[[1]][2])
-    }
-  }
-  if(cols[1] == "IDHOUS") {
-    cols[1] <- paste0(cols[1], as.character(sprintf('%02d', year %% 100)))
-    for (i in 2:length(cols)) {
-      cols[i] <- paste0(stringr::str_split(cols[i], "YY")[[1]][1], as.character(sprintf('%02d', year %% 100)), stringr::str_split(cols[i], "YY")[[1]][2])
 
-      #cols[i] <- paste0(stringr::str_sub(cols[i], 1, 1), as.character(sprintf('%02d', year %% 100)), stringr::str_sub(cols[i], 4))
-    }
-  }
 
 
 
-  assertthat::assert_that(assertthat::is.string(cols[1]))
-  return(cols)
-}
 
 
 
 
 
 
-depression <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYC17"), year_start = "2004", year_end = "2018")
+depression <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYC17"), year_start = "2004", year_end = "2018")
 
-ermuedung <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYF51"), year_start = "2004", year_end = "2018")
+ermuedung <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYF51"), year_start = "2004", year_end = "2018")
 
-stress_arbeit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW604"), year_start = "2004", year_end = "2018")
+stress_arbeit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW604"), year_start = "2004", year_end = "2018")
 
-gesundheitliche_erkrankung <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYC04A"), year_start = "2004", year_end = "2018")
+gesundheitliche_erkrankung <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYC04A"), year_start = "2004", year_end = "2018")
 
-arbeit_zufriedenheit_aufgaben <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW229"), year_start = "2004", year_end = "2018")
+arbeit_zufriedenheit_aufgaben <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW229"), year_start = "2004", year_end = "2018")
 
-art_arbeitszeit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW71A"), year_start = "2004", year_end = "2018")
+art_arbeitszeit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW71A"), year_start = "2004", year_end = "2018")
 
-einbezug_entscheidungen <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW91"), year_start = "2004", year_end = "2018")
+einbezug_entscheidungen <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW91"), year_start = "2004", year_end = "2018")
 
-arbeitskontrolle_personen <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW87"), year_start = "2004", year_end = "2018")
+arbeitskontrolle_personen <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW87"), year_start = "2004", year_end = "2018")
 
-qualifikation_arbeit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW100"), year_start = "2004", year_end = "2018")
+qualifikation_arbeit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW100"), year_start = "2004", year_end = "2018")
 
-arbeitsstunden_woche <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW77"), year_start = "2004", year_end = "2018")
+arbeitsstunden_woche <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW77"), year_start = "2004", year_end = "2018")
 
-wochenend_arbeit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW218"), year_start = "2004", year_end = "2018")
+wochenend_arbeit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW218"), year_start = "2004", year_end = "2018")
 
-nachtarbeit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW216"), year_start = "2004", year_end = "2018")
+nachtarbeit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW216"), year_start = "2004", year_end = "2018")
 
-taegl_pendel_in_min <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW84"), year_start = "2004", year_end = "2018")
+taegl_pendel_in_min <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW84"), year_start = "2004", year_end = "2018")
 
-arbeits_intensität <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW603"), year_start = "2004", year_end = "2018")
+arbeits_intensität <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW603"), year_start = "2004", year_end = "2018")
 
-zufriedenheit_arbeitsatmosphaere <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW94"),  year_start = "2004", year_end = "2018")
+zufriedenheit_arbeitsatmosphaere <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW94"),  year_start = "2004", year_end = "2018")
 
-arbeit_laerm_schmutz <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW605"), year_start = "2004", year_end = "2018")
+arbeit_laerm_schmutz <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW605"), year_start = "2004", year_end = "2018")
 
-arbeit_ermuedende_koerperha <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYW606"), year_start = "2004", year_end = "2018")
+arbeit_ermuedende_koerperha <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYW606"), year_start = "2004", year_end = "2018")
 
-hausarbeit_stunden_woche <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYF08"), year_start = "2004", year_end = "2018")
+hausarbeit_stunden_woche <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYF08"), year_start = "2004", year_end = "2018")
 
-beeintraechtigung_arbeit_privat <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYF50"), year_start = "2004", year_end = "2018")
+beeintraechtigung_arbeit_privat <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYF50"), year_start = "2004", year_end = "2018")
 
-abschalten_nach_arbeit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYF52"), year_start = "2004", year_end = "2018")
+abschalten_nach_arbeit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYF52"), year_start = "2004", year_end = "2018")
 
-einschraenkung_weg_ges_zustand <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYC08"), year_start = "2004", year_end = "2018")
+einschraenkung_weg_ges_zustand <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYC08"), year_start = "2004", year_end = "2018")
 
-tage_gesunheits_prob <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYC11"), year_start = "2004", year_end = "2018")
+tage_gesunheits_prob <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYC11"), year_start = "2004", year_end = "2018")
 
-chronische_krankheit <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYC19A"), year_start = "2004", year_end = "2018")
+chronische_krankheit <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYC19A"), year_start = "2004", year_end = "2018")
 
-geschlecht <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","SEXYY"),year_start = "2004", year_end = "2018")
+geschlecht <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","SEXYY"),year_start = "2004", year_end = "2018")
 
-hoechster_bildungsabschluss <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","EDUCATYY"), year_start = "2004", year_end = "2018")
+hoechster_bildungsabschluss <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","EDUCATYY"), year_start = "2004", year_end = "2018")
 
-partnerschaft <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYD29"), year_start = "2004", year_end = "2018")
+partnerschaft <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYD29"), year_start = "2004", year_end = "2018")
 
-tod_person <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYL11"), year_start = "2004", year_end = "2018")
+tod_person <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYL11"), year_start = "2004", year_end = "2018")
 
-person_haushalt<- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","IDHOUSYY"), year_start = "2004", year_end = "2018")
+person_haushalt<- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","IDHOUSYY"), year_start = "2004", year_end = "2018")
 
-haushaltsaequivalenzeinkommen <- import_long_cols("H_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDHOUS","IYYEQON"), year_start = "2004", year_end = "2018")
+haushaltsaequivalenzeinkommen <- import_long_cols("H_USER.sav", load_path, cols = c("IDHOUS","IYYEQON"), year_start = "2004", year_end = "2018")
 
-#schweizerstaatsbürgerschaft <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYD19"), year_start = "2004", year_end = "2018")
+#schweizerstaatsbürgerschaft <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYD19"), year_start = "2004", year_end = "2018")
 
-migrationshintergrund <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","PYYD160"),  year_start = "2004", year_end = "2018")
+migrationshintergrund <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","PYYD160"),  year_start = "2004", year_end = "2018")
 
-#pflege_angehoerige <- import_long_cols("H_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDHOUS","HYYF74"),  year_start = "2004", year_end = "2012")
+#pflege_angehoerige <- import_long_cols("H_USER.sav", load_path, cols = c("IDHOUS","HYYF74"),  year_start = "2004", year_end = "2012")
 
-haushaltstyp <- import_long_cols("H_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDHOUS","HLDTYPYY"), year_start = "2004", year_end = "2018")
+haushaltstyp <- import_long_cols("H_USER.sav", load_path, cols = c("IDHOUS","HLDTYPYY"), year_start = "2004", year_end = "2018")
 
-alter <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","AGEYY"),  year_start = "2004", year_end = "2018")
+alter <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","AGEYY"),  year_start = "2004", year_end = "2018")
 
-status <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","STATUSYY"),  year_start = "2004", year_end = "2018")
+status <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","STATUSYY"),  year_start = "2004", year_end = "2018")
 
-beschaeftigung <- import_long_cols("P_USER.sav", "FHS/5. Semester/BATHV/dataset_932 (2)/Data_SPSS/Data_SPSS/SHP-Data-W1-W20-SPSS/", cols = c("IDPERS","OCCUPAYY"),  year_start = "2004", year_end = "2018")
+beschaeftigung <- import_long_cols("P_USER.sav", load_path, cols = c("IDPERS","OCCUPAYY"),  year_start = "2004", year_end = "2018")
 
 
 schweizerstaatsbürgerschaft <- SHP18_P_USER %>% select(P18D1) # PDYYD19 gibt es gar nicht
