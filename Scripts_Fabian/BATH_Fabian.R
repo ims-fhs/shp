3622905791f41bc32e7eddaa7398492b4275025d
Fabian Streb <fabian.streb@ost.ch>
2021-03-19 09:22:47 +0100

    Erste Versuche mit kmeans

--- Scripts_Fabian/BATH_Fabian.R
+++ Scripts_Fabian/BATH_Fabian.R
@@ -15,9 +15,53 @@ sample %>% group_by(ID) %>% summarise(n = n())
 ggplot(sample %>% group_by(ID) %>% summarise(n = n())) +
   geom_bar(aes(x = n))
 
-versuch <- kmeans(sample %>% select(c(ID, PC17, year)) %>% drop_na(), 4)
+versuch <- sample %>% select(c(ID, PC17, year)) %>%
+  pivot_wider(names_from = year, values_from = PC17) %>% drop_na() %>% subset(select = -c(ID))
 
+versuch <- kmeans(versuch, 4)
 fuer_plot <- data.frame(versuch$center)
+fuer_plot <- t(fuer_plot)
+Jahr = data.frame(year=c(2004:2018))
+fuer_plot <- fuer_plot %>% cbind(year = Jahr)
+names(fuer_plot)[names(fuer_plot) == '1'] <- 'line_1'
+names(fuer_plot)[names(fuer_plot) == '2'] <- 'line_2'
+names(fuer_plot)[names(fuer_plot) == '3'] <- 'line_3'
+names(fuer_plot)[names(fuer_plot) == '4'] <- 'line_4'
+
+ggplot(fuer_plot, aes(x = year)) +
+  geom_line(aes(y = line_1), color = "darkred") +
+  geom_line(aes(y = line_2), color="steelblue") +
+  geom_line(aes(y = line_3), color="yellow") +
+  geom_line(aes(y = line_4), color="green")
+
+
+
+#install.packages("kmlShape")
+library("kmlShape")
+
+#install.packages("dplyr")
+library(dplyr)
+library(tidyverse)
+#install.packages("tidyr")
+library(tidyr)
+
+
+
+versuch2 <- sample %>% select(c(ID, PC17, year)) %>%
+  pivot_wider(names_from = year, values_from = PC17) %>% drop_na()
+
+myClds <- cldsWide(data.frame(versuch2))
+
+myClds <- reduceNbId(trajWide = data.frame(fuer_plot), nbSenators = 10, imputationMethod = "linearInterpol")
+
+kmlShape(myClds, nbClusters = 4, timeScale = 0.1, FrechetSumOrMax =
+           "max", toPlot="both", parAlgo=parKmlShape())
+
+
+
+
+
+
 
 
 
@@ -56,7 +100,7 @@ Means <- as.data.frame(Means)
 Means
 #liste <- list(dt_mit_na, dt)
 
-write_xlsx(x=haushaltstyp, path = "means.xlsx", col_names=TRUE, format_headers = TRUE)
+write_xlsx(x=haushaltstyp, path = "data/means.xlsx", col_names=TRUE, format_headers = TRUE)
 
 
 
